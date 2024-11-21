7d6ecab7a158c799486603c180d6429cc0314724
Kashif Ullah <43569363+kaashe@users.noreply.github.com>
2024-11-21 13:45:26 +0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
@@ -6,7 +7,29 @@ import theme from 'config/theme'
 import FavIconProvider from 'components/Misc/FavIconProvider'
 
 function WolvesPackSite({ Component, pageProps }: AppProps): JSX.Element {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  // Track mouse position
+  useEffect(() => {
+    const handleMouseMove = (event: any) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    // Cleanup event listener
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
   return (
+    <>
+       <div
+      className="circle"
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
+    </div>
     <AnimatePresence exitBeforeEnter>
       <ChakraProvider theme={theme}>
         <FavIconProvider>
@@ -14,6 +37,7 @@ function WolvesPackSite({ Component, pageProps }: AppProps): JSX.Element {
         </FavIconProvider>
       </ChakraProvider>
     </AnimatePresence>
+      </>
   )
 }
 export default WolvesPackSite
